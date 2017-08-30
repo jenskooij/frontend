badfbeef760e3f1e88faf8f1b73f1828df816b07
Jens Kooij <jens.kooij@incentro.com>
2017-08-30 13:14:35 +0200

    Made proper module

--- index.js
+++ index.js
@@ -7,55 +7,56 @@ const cssMinify = require('./build/cssMinify');
 const jsConcat = require('./build/jsConcat');
 const jsMinify = require('./build/jsMinify');
 
-/* 
+module.exports = {
+  sassPath: 'scss',
+  jsPath: 'js',
+  targetPath: 'target',
+  targetSassFile: 'site.css',
+  targetCssFile: 'site.min.css',
+  targetJsFile: 'site.js',
+  targetJsMinFile: 'site.min.js',
+  run: function () {
+    /*
 	Configuration
  */
-let sassPath = 'scss';
-let jsPath = 'js';
-let targetPath = 'target';
-let targetSassFile = 'site.css';
-let targetCssFile = 'site.min.css';
-let targetJsFile = 'site.js';
-let targetJsMinFile = 'site.min.js';
+    function init () {
+      "use strict";
 
-function init () {
-  "use strict";
+      // Sass
+      let sassCompileComplete = new Promise((resolve, reject) => {
+        sass.run(resolve, sassPath, targetPath, targetSassFile);
+      });
 
-  // Sass
-  let sassCompileComplete = new Promise((resolve, reject) => {
-    sass.run(resolve, sassPath, targetPath, targetSassFile);
-  });
+      sassCompileComplete.then((successMessage) => {
+        console.log(successMessage);
 
-  sassCompileComplete.then((successMessage) => {
-    console.log(successMessage);
+        // Minify
+        let cssMinifyComplete = new Promise((resolve, reject) => {
+          cssMinify.run(resolve, targetPath, targetSassFile, targetCssFile);
+        });
 
-    // Minify
-    let cssMinifyComplete = new Promise((resolve, reject) => {
-      cssMinify.run(resolve, targetPath, targetSassFile, targetCssFile);
-    });
+        cssMinifyComplete.then((successMessage) => {
+          console.log(successMessage);
+        });
+      });
 
-    cssMinifyComplete.then((successMessage) => {
-      console.log(successMessage);
-    });
-  });
+      // Js
+      let jsConcatComplete = new Promise((resolve, reject) => {
+        jsConcat.run(resolve, jsPath, targetPath, targetJsFile);
+      });
 
-  // Js
-  let jsConcatComplete = new Promise((resolve, reject) => {
-    jsConcat.run(resolve, jsPath, targetPath, targetJsFile);
-  });
+      jsConcatComplete.then((successMessage) => {
+        console.log(successMessage);
+        let jsMinifyComplete = new Promise((resolve, reject) => {
+          jsMinify.run(resolve, targetPath, targetJsFile, targetJsMinFile);
+        });
 
-  jsConcatComplete.then((successMessage) => {
-    console.log(successMessage);
-    let jsMinifyComplete = new Promise((resolve, reject) => {
-      jsMinify.run(resolve, targetPath, targetJsFile, targetJsMinFile);
-    });
-
-    jsMinifyComplete.then((successMessage) => {
-      console.log(successMessage);
-    });
-  });
+        jsMinifyComplete.then((successMessage) => {
+          console.log(successMessage);
+        });
+      });
 
 
-}
-
-init();
+    }
+  }
+};
